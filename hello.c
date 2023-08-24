ced25bac7984c4a95e00a315fc256526b8186b1a
Alec Whitten <awhitten2@huskers.unl.edu>
2023-08-24 12:58:40 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Alec Whitten
+ * Date: 2023/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Alec Whitten\n");
+
+  return 0;
+}
